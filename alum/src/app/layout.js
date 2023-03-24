891568de25b54898a0fe65c7fc4dd56299d0603d
Arnav Gupta <info@arnavgupta.net>
2023-03-25 03:26:08 +0530

    Alum customized

--- alum/src/app/layout.js
+++ alum/src/app/layout.js
@@ -7,12 +7,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <nav></nav>
+      <body>
+        <main>{children}</main>
+      </body>
+      <footer></footer>
     </html>
   );
 }
