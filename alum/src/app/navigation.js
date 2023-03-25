5022f20acb69f47e54c337cbf833b81216f31d4e
Arnav Gupta <info@arnavgupta.net>
2023-03-26 04:20:26 +0530

    Synamic navigation

--- alum/src/app/navigation.js
+++ alum/src/app/navigation.js
@@ -2,7 +2,11 @@
 
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import Link from "next/link";
+import Admin from "./components/admin";
+import Alumni from "./components/alumni";
+import LoggedOut from "./components/logged-out";
+import Pending from "./components/pending";
+import Student from "./components/student";
 
 export default function Navigation({ data }) {
   let path = usePathname().toLowerCase();
@@ -16,32 +20,21 @@ export default function Navigation({ data }) {
         alt="Logo of the Alum portal"
       ></Image>
       <div className="nav-title">ALUM</div>
-      <div className="nav-links">
-        {[
-          ["Home", "/"],
-          ["About", "/about"],
-          ["Register", "/register"],
-        ].map((e) => {
-          return (
-            <div
-              key={e[0].toLowerCase()}
-              className={`nav-sub-links ${
-                e[1].toLowerCase() == path.toLowerCase() && "active"
-              } ${e[0] == "Register" && "small"}`}
-            >
-              <Link href={e[1].toLowerCase()}>{e[0]}</Link>
-            </div>
-          );
-        })}
-        <Link
-          href="/login"
-          className={`nav-sub-button ${
-            path.toLowerCase() == "/login" && "active"
-          }`}
-        >
-          <div>Login</div>
-        </Link>
-      </div>
+      {data.loggedIn ? (
+        data.data.verified ? (
+          data.data.type == "student" ? (
+            <Student></Student>
+          ) : data.data.type == "alumni" ? (
+            <Alumni></Alumni>
+          ) : (
+            <Admin></Admin>
+          )
+        ) : (
+          <Pending></Pending>
+        )
+      ) : (
+        <LoggedOut path={path}></LoggedOut>
+      )}
     </nav>
   );
 }
--- alum/src/app/page.js
+++ alum/src/app/page.js
@@ -1,9 +1,18 @@
-import LoggedIn from "./loggedIn";
+import Link from "next/link";
+import "./home.css";
 
 export default function Home() {
   return (
     <main>
-      <div className="banner"></div>
+      <div className="banner">
+        <div className="content">
+          <div className="heading">ALUM</div>
+          <div className="description">The NSUT networking site</div>
+          <Link className="button" href="/register">
+            Register
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
