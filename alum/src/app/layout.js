660921e37c28eb3d408f67a896f68f9419b03125
Arnav Gupta <info@arnavgupta.net>
2023-03-25 15:09:09 +0530

    CSS

--- alum/src/app/layout.js
+++ alum/src/app/layout.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import "./globals.css";
 
 export const metadata = {
@@ -8,7 +9,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <nav></nav>
+      <nav>
+        <Image
+          className="logo"
+          src="/logo.png"
+          width={30}
+          height={30}
+          alt="Logo of the Alum portal"
+        ></Image>
+        <div className="nav-title">ALUM</div>
+        <div className="nav-links">
+          <div className="nav-sub-links">Home</div>
+          <div className="nav-sub-links">About</div>
+          <div className="nav-sub-links">Register</div>
+          <div className="nav-sub-button">Login</div>
+        </div>
+      </nav>
       <body>
         <main>{children}</main>
       </body>
