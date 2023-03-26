08416d986f7ef674ee52446ba9c3268a1054fcda
Arnav Gupta <info@arnavgupta.net>
2023-03-26 05:39:49 +0530

    Removed banner

--- alum/src/app/page.js
+++ alum/src/app/page.js
@@ -2,17 +2,5 @@ import Link from "next/link";
 import "./home.css";
 
 export default function Home() {
-  return (
-    <main>
-      <div className="banner">
-        <div className="content">
-          <div className="heading">ALUM</div>
-          <div className="description">The NSUT networking site</div>
-          <Link className="button" href="/register">
-            Register
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
