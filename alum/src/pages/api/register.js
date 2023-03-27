6dcd4f4232397cfb473d9863c36234094c156bc7
Arnav Gupta <info@arnavgupta.net>
2023-03-28 03:25:56 +0530

    Registeration issue in realm resolved

--- alum/src/pages/api/register.js
+++ alum/src/pages/api/register.js
@@ -1,12 +1,10 @@
-import Realm from "realm";
 import bcrypt from "bcrypt";
+import * as Realm from "realm-web";
 
 export default async function contact(req, res) {
   let body = JSON.parse(req.body);
   body.email = body.email.replaceAll('"', "'").replaceAll("\n", " ");
-  const app = new Realm.App({
-    id: process.env.APP_ID,
-  });
+  const app = Realm.getApp(process.env.APP_ID);
   try {
     const data = await fetch(process.env.GRAPHQL_URI, {
       method: "POST",
