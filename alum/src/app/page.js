bb4d906b42170b2e70e37f7290fa1b1611ed458c
Arnav Gupta <info@arnavgupta.net>
2023-04-04 23:46:11 +0530

    new banner

--- alum/src/app/page.js
+++ alum/src/app/page.js
@@ -5,18 +5,7 @@ export default function Home() {
   return (
     <main>
       <div className="banner">
-        <div className="banner-overlay">
-          <div className="title">Alum</div>
-          <div className="description">
-            Welcome to the NSUT Alumni Portal, your gateway to reconnecting with
-            your alma mater and fellow graduates, staying up-to-date on the
-            latest news and events, and accessing valuable career resources and
-            networking opportunities.
-          </div>
-          <Link className="button" href="/register">
-            Register Now
-          </Link>
-        </div>
+        <div className="content-container"></div>
       </div>
     </main>
   );
