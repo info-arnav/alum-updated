fa9e8598ea09b045b554b22805b9e05a77c51757
Arnav Gupta <info@arnavgupta.net>
2023-04-05 16:59:58 +0530

    Privacy Policy Updated

---
+++ alum/src/app/privacy-policy/dropdown.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import "./dropdown.css";
+export default function Dropdown({ title, content }) {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <hr></hr>
+      <div className="dropdown-header" onClick={(e) => setShow(!show)}>
+        <div className="dropdown-title">{title}</div>
+        <div className="dropdown-icons">{show ? "x" : "+"}</div>
+      </div>
+      {show && (
+        <div
+          className="dropdown-content"
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></div>
+      )}
+    </>
+  );
+}
