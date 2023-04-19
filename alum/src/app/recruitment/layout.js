1550e18b0d654a7091f6ac4af67d0b0f52e89b83
Arnav Gupta <info@arnavgupta.net>
2023-04-19 19:43:41 +0530

    meta error check

--- alum/src/app/recruitment/layout.js
+++ alum/src/app/recruitment/layout.js
@@ -4,43 +4,45 @@ import LoggedIn from "../loggedIn";
 const status = LoggedIn();
 
 let description = (status.data.type = "student"
-  ? "Explore inspiring stories of NSUT alumni who have made remarkable strides in their careers. Learn about their recruitment journeys, companies they've joined, and how they've utilized their NSUT education to excel professionally."
-  : "Looking for a platform where NSUT alumni can easily manage their job recruitment requests? Check out our recruitment page on Alum, the alumni portal on NSUT! Here, alumni can view, edit, and delete all of their recruitment requests, as well as monitor their status. Join Alum today and take control of your career opportunities as an NSUT alumnus.");
+  ? "Effortlessly manage your recruitment posts on Alum, the NSUT alumni portal. Create, edit, or delete job opportunities, and monitor their status to streamline your hiring process and attract top talent."
+  : "Explore exciting job opportunities on Alum, the NSUT alumni portal. Browse and apply for positions posted by esteemed alumni, and kickstart your career with valuable connections in your professional network.");
 
-export const metadata = {
-  title: "View Recruitments",
-  description: description,
-  openGraph: {
+export async function metadata() {
+  return {
     title: "View Recruitments",
     description: description,
-    url: `${process.env.LINK}recruitment`,
-    siteName: "Alum",
-    images: [
-      {
-        url: `${process.env.LINK}logo-background.png`,
-        width: 250,
-        height: 250,
-        alt: "Alum - NSUT Alumni Network Logo",
-      },
-      {
-        url: `${process.env.LINK}logo.png`,
-        width: 250,
-        height: 193,
-        alt: "Alum - NSUT Alumni Network Logo",
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  twitter: {
-    title: "View Recruitments",
-    description: description,
-    images: [
-      `${process.env.LINK}logo-background.png`,
-      `${process.env.LINK}logo.png`,
-    ],
-  },
-};
+    openGraph: {
+      title: "View Recruitments",
+      description: description,
+      url: `${process.env.LINK}recruitment`,
+      siteName: "Alum",
+      images: [
+        {
+          url: `${process.env.LINK}logo-background.png`,
+          width: 250,
+          height: 250,
+          alt: "Alum - NSUT Alumni Network Logo",
+        },
+        {
+          url: `${process.env.LINK}logo.png`,
+          width: 250,
+          height: 193,
+          alt: "Alum - NSUT Alumni Network Logo",
+        },
+      ],
+      locale: "en-US",
+      type: "website",
+    },
+    twitter: {
+      title: "View Recruitments",
+      description: description,
+      images: [
+        `${process.env.LINK}logo-background.png`,
+        `${process.env.LINK}logo.png`,
+      ],
+    },
+  };
+}
 
 export default function RecruitmentPage({ children }) {
   if (!status.loggedIn) {
