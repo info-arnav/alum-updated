5ab56478d943403fb8df3ba4e118b933a2537e18
Arnav Gupta <info@arnavgupta.net>
2023-04-19 15:18:04 +0530

    Initials for recruitment page

---
+++ alum/src/app/recruitment/new/layout.js
@@ -0,0 +1,53 @@
+import Empty from "@/app/empty";
+import LoggedIn from "@/app/loggedIn";
+
+export const metadata = {
+  title: "Login",
+  description:
+    "Log in to your Alum account to connect with NSUT students and alumni, explore networking opportunities, find job offers, internships, and engage in mentorship within the NSUT community.",
+  openGraph: {
+    title: "Login",
+    description:
+      "Log in to your Alum account to connect with NSUT students and alumni, explore networking opportunities, find job offers, internships, and engage in mentorship within the NSUT community.",
+    url: `${process.env.LINK}login`,
+    siteName: "Alum",
+    images: [
+      {
+        url: `${process.env.LINK}logo-background.png`,
+        width: 250,
+        height: 250,
+        alt: "Alum - NSUT Alumni Network Logo",
+      },
+      {
+        url: `${process.env.LINK}logo.png`,
+        width: 250,
+        height: 193,
+        alt: "Alum - NSUT Alumni Network Logo",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    title: "Login",
+    description:
+      "Log in to your Alum account to connect with NSUT students and alumni, explore networking opportunities, find job offers, internships, and engage in mentorship within the NSUT community.",
+    images: [
+      `${process.env.LINK}logo-background.png`,
+      `${process.env.LINK}logo.png`,
+    ],
+  },
+};
+
+export default function RecruitmentNewPage({ children }) {
+  const status = LoggedIn();
+  if (status.loggedIn) {
+    return (
+      <>
+        <Empty link="/"></Empty>
+      </>
+    );
+  } else {
+    return <>{children}</>;
+  }
+}
