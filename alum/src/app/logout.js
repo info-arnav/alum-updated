1fcd4fc23accf5ccf98e77c9e9ae4065df1365d9
Arnav Gupta <info@arnavgupta.net>
2023-04-23 22:49:10 +0530

    Test

--- alum/src/app/logout.js
+++ alum/src/app/logout.js
@@ -2,7 +2,5 @@ import Cookies from "universal-cookie";
 
 export default async function Logout() {
   let cookies = new Cookies();
-  await fetch("/api/logout")
-    .then((e) => cookies.remove("User"))
-    .then((e) => location.reload());
+  await fetch("/api/logout").then((e) => cookies.remove("User"));
 }
