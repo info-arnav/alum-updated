97a524683c2003ae932079511171e28ae4e7fb12
Arnav Gupta <info@arnavgupta.net>
2023-04-24 00:44:47 +0530

    Cookie test

--- alum/src/pages/api/logout.js
+++ alum/src/pages/api/logout.js
@@ -1,8 +1,8 @@
-export default async function login(req, res) {
+export default async function logout(req, res) {
   try {
     res.setHeader(
       "Set-Cookie",
-      `login_token=; Max-Age=0; expires=Thu, 01 Jan 1970 00:00:00 UTC; HttpOnly; Path=/`
+      `login_token=; Expires=Thu, 01 Jan 1970 00:00:01 GMT'; Max-Age=0; HttpOnly; Secure; SameSite=lax; Path=/`
     );
     res.json({ loggedOut: true });
   } catch {
