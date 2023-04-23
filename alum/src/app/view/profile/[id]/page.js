a070e9c64e0409ed0672b5e045a8c325a7872b95
Arnav Gupta <info@arnavgupta.net>
2023-04-23 20:32:10 +0530

    If viewing your profile in others profile, view edit options too

--- alum/src/app/view/profile/[id]/page.js
+++ alum/src/app/view/profile/[id]/page.js
@@ -1,15 +1,11 @@
 import LoggedIn from "@/app/loggedIn";
-import UserProfile from "./userProfile";
+import Check from "./check";
 
 export default function Profile({ params }) {
   const data = LoggedIn();
   return (
     <div className="profile-page">
-      <UserProfile
-        id={params.id}
-        link={process.env.LINK}
-        data={data}
-      ></UserProfile>
+      <Check id={params.id} link={process.env.LINK} data={data}></Check>
     </div>
   );
 }
--- alum/src/pages/api/get-user-info.js
+++ alum/src/pages/api/get-user-info.js
@@ -24,6 +24,7 @@ query{
     batch,
     bio,
     branch,
+    email,
     name,
     occupation,
     education,
