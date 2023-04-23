173449bca480c9c92e9b39c2ea4242c8effeffb6
Arnav Gupta <info@arnavgupta.net>
2023-04-24 01:51:02 +0530

    Small bug fixed

--- alum/src/app/logout.js
+++ alum/src/app/logout.js
@@ -1,10 +1,11 @@
-"use client";
-
 import Cookies from "universal-cookie";
 
 export default async function Logout() {
   let cookies = new Cookies();
-  await fetch("/api/logout")
+  await fetch("/api/logout", {
+    method: "POST",
+    body: JSON.stringify({}),
+  })
     .then((e) =>
       cookies.remove("session_id", {
         path: "/",
