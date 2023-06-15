268469ba5ac6251294e5821b0947ede771fd4d65
Chirag <chiragsaini6405@gmail.com>
2023-06-15 22:05:28 +0530

    Co-authored-by: codingveenit <codingveenit@users.noreply.github.com> Co-authored-by: Rohit Kumar Shaw <RohitShaw340@users.noreply.github.com>

---
+++ alum/src/app/recruitment/Read.js
@@ -0,0 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Loading from "../home/loading";
+import Apply from "../view/recruitment/[id]/apply";
+
+const Read = (props) => {
+  console.log(props.info);
+  
+  return (
+    <div>
+    {(props.info._id?(<div className="m-4">
+
+        <div className="m-2 grid grid-cols-2">
+          <div className="">
+            <h1 className="font-bold text-2xl">{props.info.title}</h1>
+            <h5 className="font-bold text-lg">{props.info.company}</h5>
+          </div>
+          <div className="recruitment-box-footer">
+            <Apply
+              recruitment={props.info._id}
+              user={props.email}
+              applicants={props.info.applicants}
+            ></Apply>
+          </div>
+        </div>
+
+        <div className="m-2 flex flex-row justify-between align-middle">
+        <div className=""><p className=" mr-2">Start Date </p><p className="inline">{props.info.startDate || "No data"}</p></div>
+        <div className=""><p className=" mr-2">Duration </p><p className="inline">{props.info.duration || "No data"}</p></div>
+        <div className=" mr-4"><p className=" mr-2">Stipend </p><p className="inline">{props.info.Stipend || "No data"}</p></div>
+        </div>
+
+        <div className="m-2">
+          <h5>About the Internship</h5>
+          <p dangerouslySetInnerHTML={{
+              __html: `<p></p>
+                ${
+                  props.info.description
+                }`,
+            }}
+          ></p>
+        </div>
+
+      </div>) : 
+      <div> No data </div>)}
+
+  </div>
+  )  
+}
+
+export default Read;
