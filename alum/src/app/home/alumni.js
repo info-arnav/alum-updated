9331340b64d261f64da89d9d3917d3727f9f23ad
Arnav Gupta <info@arnavgupta.net>
2023-08-13 23:46:59 +0530

    changes

--- alum/src/app/home/alumni.js
+++ alum/src/app/home/alumni.js
@@ -130,7 +130,7 @@ export default function Alumni() {
             </div>
             <div className="news my-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              no bis quae explicabo dolores laboriosam assumenda quos,
+              nobis quae explicabo dolores laboriosam assumenda quos,
               consequuntur officiis eum optio.
             </div>
           </div> */}
