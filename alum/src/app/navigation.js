29c8f645e5b8c45b856b90dd3dd40a9cd7393f0a
RohitShaw340 <rohitshaw340@gmail.com>
2023-08-14 01:54:19 +0530

    Co-authored-by: Aditya Giri <Aditya-giri03@users.noreply.github.com> Co-authored-by: ChiragSaini03 <ChiragSaini03@users.noreply.github.com>

--- alum/src/app/navigation.js
+++ alum/src/app/navigation.js
@@ -19,17 +19,29 @@ export default function Navigation({ data, keys, LINK }) {
   function Hit({ hit }) {
     console.log(hit);
     return (
-      <div classname="w-full">
+      <div classname="w-full ">
         <a href={`${LINK}/view/profile/${hit.objectID}`}>
-          <div className="flex flex-row justify-between items-center p-2 md:px-4 md:py-2">
-            <div>
+          <div className="flex flex-row  border-b-2  border-blue-900  mx-1 bg-[#F5F4FB] items-center p-2 ">
+            {/* p-2 md:px-4 md:py-2 */}
+            <div className="mr-4">
               <img
-                className="w-[50px] h-[50px]"
+                className="w-[60px] h-[60px] rounded-full"
                 src={`${LINK}/api/image/${hit.objectID}`}
               ></img>
             </div>
-            <div>{hit.name}</div>
-            <div>{hit.batch}</div>
+
+            <div className="flex flex-col w-full">
+              <div className="flex justify-between">
+                <div className=" text-lg font-semibold">{hit.name}</div>
+                <div className=" md:mr-8 text-white bg-[#1976D2] px-2 rounded-md text-center h-[70%]">
+                  {hit.batch}
+                </div>
+              </div>
+              <div
+                className="text-sm font-extralight"
+                dangerouslySetInnerHTML={{ __html: hit.bio }}
+              ></div>
+            </div>
           </div>
         </a>
       </div>
@@ -52,18 +64,19 @@ export default function Navigation({ data, keys, LINK }) {
       </a>
       {data.loggedIn && data.data.verified ? (
         value ? (
-          <div className="absolute bg-black/[.54] flex-col justify-center w-full h-[100vh] top-[60px] p-4  ">
-            <div className="card relative w-[90%] md:w-[60%] h-[80%] mx-auto bg-white">
-              <InstantSearch
-                className="color-blue"
-                searchClient={searchClient}
-                indexName="dev_alum"
-              >
+          <div className="absolute bg-black/[.54] flex-col justify-center w-full h-[100vh] top-[60px] ">
+            <div className="card overflow-y-auto rounded-lg border-2 border-black relative w-[90%] md:w-[50%] h-[80%] mx-auto bg-white py-4">
+              <InstantSearch searchClient={searchClient} indexName="dev_alum">
                 <div className="flex flex-row justify-center">
-                  <SearchBox className="ml-5" searchAsYouType={true} />
-                  <button onClick={(e) => setValue(false)}>Stop</button>
+                  <SearchBox searchAsYouType={true} />
+                  <button
+                    className="form-close"
+                    onClick={(e) => setValue(false)}
+                  >
+                    X
+                  </button>
                 </div>
-                {/* <div className="flex  text-center w-[100%]"> */}
+                {/* <div className="w-[100%] bg-blue-200"> */}
                 <Hits hitComponent={Hit} />
                 {/* </div> */}
                 {/* <SearchPopup/> */}
---
+++ alum/src/app/recruitment/Readalum.js
@@ -0,0 +1,141 @@
+"use client";
+
+import Apply from "./apply";
+import Image from "next/image";
+import idleimage from "../image/select.png";
+import Modal from "./components/modal";
+// import Loading from "../home/loading";
+// import Error from "../error";
+import Delete from "./components/delete";
+import Link from "next/link";
+
+const Readalum = (props) => {
+  return (
+    <div>
+      {props.info._id ? (
+        <div className="m-4">
+          <div className="m-2 grid grid-cols-2">
+            <div className="">
+              <h1 className="font-bold text-2xl">{props.info.title}</h1>
+              <h5 className="font-bold text-lg text-[#4358b7]">
+                {props.info.company}
+              </h5>
+            </div>
+            <div className="recruitment-box-footer sm:flex-row  flex-col">
+              {/* <Link
+                href={`/view/status/${props.info._id}`}
+                id="status"
+                className="recuit-button text-white"
+              >
+                Status
+              </Link>
+              <Modal
+                type="edit"
+                data={props.info}
+                mainData={data}
+                email={email}
+                updater={setData}
+                position={data.indexOf(e)}
+                refresh={refresh}
+                update={setRefresh}
+              ></Modal>
+              <Delete
+                updater={setData}
+                data={data}
+                position={data.indexOf(e)}
+                refresh={refresh}
+                update={setRefresh}
+                index={props.info._id}
+                email={email}
+              ></Delete>
+              <Apply
+                recruitment={props.info._id}
+                user={props.email}
+                profiles={props.profiles}
+                setProfiles={props.setProfiles}
+              ></Apply> */}
+            </div>
+          </div>
+
+          <div className="m-4 md:flex md:flex-row inline justify-between align-middle">
+            <div className=" p-2 md:p-0">
+              <p className=" mr-2">Start Date </p>
+              <p className="inline text-[#4358b7]">
+                {props.info.deadline || "No data"}
+              </p>
+            </div>
+            <div className=" p-2 md:p-0">
+              <p className=" mr-2">Contact Email </p>
+              <p className="inline text-[#4358b7]">
+                {props.info.email || "No data"}
+              </p>
+            </div>
+            <div className=" p-2 md:p-0">
+              <p className=" mr-2">Duration </p>
+              <p className="inline text-[#4358b7]">
+                {props.info.duration || "No data"}
+              </p>
+            </div>
+            <div className=" mr-4 p-2 md:p-0">
+              <p className=" mr-2">Stipend </p>
+              <p className="inline text-[#4358b7]">
+                {props.info.stipend || "No data"}
+              </p>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className="m-2">
+            <h5 className="font-bold">About the Internship</h5>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: `<p></p>
+                ${props.info.description}`,
+              }}
+            ></p>
+          </div>
+        </div>
+      ) : (
+        <div className="box-border">
+          <Image
+            src={idleimage}
+            className="p-7 w-[750px] h-[620px]"
+            alt="image login"
+          ></Image>
+        </div>
+      )}
+
+      <div className="recruitment-box-footer sm:flex-row  flex-col">
+        {/* <Link
+          href={`/view/status/${props.info._id}`}
+          id="status"
+          className="recuit-button text-white"
+        >
+          Status
+        </Link>
+        <Modal
+          type="edit"
+          data={props.info}
+          mainData={data}
+          email={email}
+          updater={setData}
+          position={data.indexOf(e)}
+          refresh={refresh}
+          update={setRefresh}
+        ></Modal>
+        <Delete
+          updater={setData}
+          data={data}
+          position={data.indexOf(e)}
+          refresh={refresh}
+          update={setRefresh}
+          index={e._id}
+          email={email}
+        ></Delete> */}
+      </div>
+    </div>
+  );
+};
+
+export default Readalum;
